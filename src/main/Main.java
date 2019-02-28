47292b4a109cf32585feff93d53bb25ad7258d0e
dp68 <dp68@st-andrews.ac.uk>
2019-02-28 18:25:29 +0000

    Correct filtering

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import util.PrintFormatting;
 import util.WrappedReader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
@@ -15,42 +16,51 @@ public class Main {
 
 
         FILES.add("files/a_example.txt");
-        FILES.add("files/b_lovely_landscapes.txt");
-        FILES.add("files/c_memorable_moments.txt");
-        FILES.add("files/d_pet_pictures.txt");
-        FILES.add("files/e_shiny_selfies.txt");
+//        FILES.add("files/b_lovely_landscapes.txt");
+//        FILES.add("files/c_memorable_moments.txt");
+//        FILES.add("files/d_pet_pictures.txt");
+//        FILES.add("files/e_shiny_selfies.txt");
 
         ArrayList<String> lines;
         for (String name : FILES) {
             lines = WrappedReader.readFileLines(name);
 
             N = Integer.parseInt(lines.get(0));
-            ArrayList<Photo> photos = new ArrayList<>();
-
-            for (int i = 1; i < lines.size(); i++) {
-
-                String[] par = lines.get(i).split(" ");
-
-                boolean h = false;
-                if (par[0].equals("H")) {
-                    h = true;
-                }
-
-                int tagsNum = Integer.parseInt(par[1]);
-                ArrayList<String> tags = new ArrayList<>();
-
-                for (int j = 2; i < par.length; i++) {
-                    tags.add(par[j]);
-                }
-                Photo p = new Photo(i - 1, h, tagsNum, tags);
-                photos.add(p);
-                PrintFormatting.print(p);
-            }
+            ArrayList<Photo> photos = getSample(lines);
+            photos = clearSingleTag(photos);
+            PrintFormatting.print(photos);
+            // TODO: 28/02/2019 All the code
         }
-
-
     }
 
+    private static ArrayList<Photo> getSample(ArrayList<String> lines) {
+        ArrayList<Photo> photos = new ArrayList<>();
+        for (int i = 1; i < lines.size(); i++) {
+
+            String[] param = lines.get(i).split(" ");
+
+            boolean h = false;
+            if (param[0].equals("H")) {
+                h = true;
+            }
+
+            int tagsNum = Integer.parseInt(param[1]);
+            ArrayList<String> tags = new ArrayList<>(Arrays.asList(param).subList(2, param.length));
+            Photo p = new Photo(i - 1, h, tagsNum, tags);
+            photos.add(p);
+        }
+        return photos;
+    }
+
+    private static ArrayList<Photo> clearSingleTag(ArrayList<Photo> inputPhotos) {
+        ArrayList<Photo> filtered = new ArrayList<>();
+        for (Photo inputPhoto : inputPhotos) {
+            if (!inputPhoto.isHorizontal() || inputPhoto.numTags != 1) {
+                filtered.add(inputPhoto);
+            }
+        }
+        return filtered;
+    }
 }
 
 
